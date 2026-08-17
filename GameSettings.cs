ff90a3efc57f9dc0b5b4c70a766a5af384d2bc75
Nanzy Halliday <jmsjunior96@gmail.com>
2026-08-16 23:34:57 -0500

    Initial commit

---
+++ GameSettings.cs
@@ -0,0 +1,4 @@
+﻿internal static class GameSettings {
+    public const int GAME_WIDTH = 90;
+    public const int GAME_HEIGHT = 30;
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder
+    .GetBuilder()
+    .SetWindowSizeInCells(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<ZeroPlayersOnline.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    .Run();
